2b77f0f64046adc83449fa4bf4dc8f05b86c3199
Xingyu-Xiao <3429787961@qq.com>
2024-12-03 20:43:34 +0800

    update assignment10

--- all the problem/assignment10/小游戏.py
+++ all the problem/assignment10/小游戏.py
@@ -3,24 +3,22 @@ dire = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
 
 def bfs(x, y, board, goal):
-    ans = []
     visited = set()
-    queue = deque([(x, y, -10, 0)])
+    min_x = float('inf')
+    queue = deque([(x, y, -1, 0)])
     while queue:
         x, y, d, num = queue.popleft()
         for i, (dx, dy) in enumerate(dire):
-            if i + d == 1 or i + d == 5:
-                continue
             nx, ny = x + dx, y + dy
             new_num = num if i == d else num + 1
             if [ny, nx] == goal:
-                ans.append(new_num)
+                min_x = min(min_x, new_num)
                 continue
             if (0 <= nx < len(board) and 0 <= ny < len(board[0]) and (nx, ny, i) not in visited and
-                    board[nx][ny] != 'X'):
+                    board[nx][ny] != 'X' and new_num < min_x):
                 visited.add((nx, ny, i))
                 queue.append((nx, ny, i, new_num))
-    return min(ans, default=None)
+    return min_x
 
 
 n = 0
@@ -40,7 +38,7 @@ while True:
         ca1 = card[0:2]
         ca2 = card[2:]
         k = bfs(ca1[1], ca1[0], bo, ca2)
-        if k:
+        if k != float('inf'):
             print(f'Pair {m}: {k} segments.')
         else:
             print(f'Pair {m}: impossible.')
