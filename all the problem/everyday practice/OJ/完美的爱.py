008e77c0365089485356ed53d87b2315bb46f030
Xingyu-Xiao <3429787961@qq.com>
2024-12-22 08:45:07 +0800

    update 每日选做

--- all the problem/everyday practice/OJ/完美的爱.py
+++ all the problem/everyday practice/OJ/完美的爱.py
@@ -1,2 +1,12 @@
 n = int(input())
 gifts = list(map(lambda x: int(x)-520, input().split()))
+prefix_sum = 0
+ps = {0: -1}
+length = 0
+for i in range(n):
+    prefix_sum += gifts[i]
+    if prefix_sum in ps:
+        length = max(i - ps[prefix_sum], length)
+    else:
+        ps[prefix_sum] = i
+print(length*520)
